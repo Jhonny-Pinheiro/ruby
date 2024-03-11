b094a76e8bae860d9a65448ac07650c6a020ed0a
Jhonny Pinheiro+ <jhonny.saletty@gmail.com>
2024-03-11 08:17:20 -0300

    iterations lessons

---
+++ time.rb
@@ -0,0 +1,15 @@
+# 6.times {print "Oi "}
+
+# 3.times {puts "Olá"}
+
+# 4.times do
+#     puts "Hello"
+# end
+
+# 2.times do
+#     print "Hola "
+# end
+
+20.times do |contador|
+    puts "#{contador}"
+end
